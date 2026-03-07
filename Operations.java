720676fcd337edc68caef613f764ed64f3103e98
yossef <yossefahmed6332@gmail.com>
2026-03-07 23:08:05 +0200

    Refactor withdraw and deposit methods with security checks

--- Operations.java
+++ Operations.java
@@ -1,75 +1,34 @@
-import java.util.ArrayList;
-
-class Operations{
-    private ArrayList<Double> Values;
-    private ArrayList<String> Operations;
-    private double balance ;
-
-    public Operations(){//set all elements as null or zero
-        balance=0;
-        Values=new ArrayList<>();
-        Operations=new ArrayList<>();
-    }
+ class Operations{
 
 
-    public boolean check_withdraw(double amount){
-        if(amount>balance){//if user amount more than balance
-            System.out.println("You can't withdraw more than your Balance");
-            return false;
-        }
-        else if(amount<=0){//if user amount less than 0
-            System.out.println("You can't withdraw less than or equal 0");
-            return false;
-        }
-        return true;
-    }
 
-    public void withdraw(double amount){
-        if(!check_withdraw(amount)){
-            System.out.print(" operation failed");
-        }
-        else {
-            this.balance -=amount;
-            Operations.add("Withdraw");
-            Values.add(amount);
-            System.out.println("withdraw successfully");
+
+
+        public void withdraw(double amount,Security sec,User_Details user){
+            if(sec.check_Withdraw(amount,user.getBalance())){
+                user.withdraw(amount,user);
+                Transaction transaction=new Transaction("Withdrawal",amount,user);
+                user.AddTransaction(transaction);
+                return;
+            }
+            System.out.println("invalid operation");
+            return;
+
         }
 
-    }
-    public boolean check_diposite(double amount){
+         void deposit(double amount,Security sec,User_Details user){
+            if(sec.check_Deposit(amount,user.getBalance())){
+                user.diposit(amount,user);
+                Transaction transaction=new Transaction("Withdrawal",amount,user);
+                user.AddTransaction(transaction);
+                return;
+            }
+             System.out.println("invalid operation");
+             return;
+            }
 
-        if(amount<=0){
-            System.out.println("You can't diposite less than or equal 0");
-            return false;
-        }
-        else
-            return true;
-    }
 
-    public void deposit(double amount){
 
-        if (!check_diposite(amount)){
-            System.out.print(" operation failed");
-        }
-        else{
-            this.balance +=amount;
-            Operations.add("Deposit");
-            Values.add(amount);
-            System.out.println("diposit successfully");
-        }
-        System.out.println("Balance is "+this.balance);
+
     }
 
-
-    public String getBalance(){//if user want to see balance
-        return String.valueOf(balance);
-
-    }
-    public ArrayList<String> getOperations(){//method to get operations
-        return Operations;
-    }
-    public ArrayList<Double> getValues(){//method to get values
-        return Values;
-    }
-
-}
