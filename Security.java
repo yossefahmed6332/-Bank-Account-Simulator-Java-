2a35eed16bd197a3e8997bf3520a4101373ee01d
yossef <yossefahmed6332@gmail.com>
2026-03-07 23:09:55 +0200

    Refactor Security class and add amount checking methods

--- Security.java
+++ Security.java
@@ -1,12 +1,20 @@
 
-public class Security {
+ class Security {
 
-
-     static boolean CheckPassword(String Password,User_Details user){
+    //methods to check PIN , password
+     boolean CheckPassword(String Password,User_Details user){
         return Password. equals(user.getPassword());
     }
-    static boolean CheckPIN (String PIN , User_Details user){
-         return PIN.equals(user.getPIN());
+     boolean CheckPIN (String PIN , User_Details user){
+        return PIN.equals(user.getPIN());
+    }
+
+    //methods for check amount
+    boolean check_Withdraw(double amount,double balance){
+         return amount >=0 && amount >=balance;
+    }
+    boolean check_Deposit(double amount,double balance){
+         return amount >=0;
     }
 
 
