b652a1e8dea2a05b71b64684e6c1076443f13087
yossef <yossefahmed6332@gmail.com>
2026-03-09 01:30:20 +0200

    Update Security.java

--- Security.java
+++ Security.java
@@ -1,5 +1,5 @@
 
- class Security {
+public class Security {
 
     //methods to check PIN , password
      boolean CheckPassword(String Password,User_Details user){
@@ -11,10 +11,16 @@
 
     //methods for check amount
     boolean check_Withdraw(double amount,double balance){
-         return amount >=0 && amount >=balance;
+         return amount >=0 && amount <=balance;
     }
     boolean check_Deposit(double amount,double balance){
          return amount >=0;
+    }
+    boolean Repeated (String checked){
+        for (int i=0; i<User_Details.users.size(); i++)
+            if (checked.equals(User_Details.users.get(i).getAccount_number()))return true;
+        return false;
+
     }
 
 
