1ee817344d828e6e02695781992525101a540348
yossef <yossefahmed6332@gmail.com>
2026-03-15 22:24:08 +0200

    Refactor Operations methods for clarity and correctness

--- Operations.java
+++ Operations.java
@@ -7,9 +7,8 @@
 
         public void withdraw(double amount,Security sec,User_Details user,String PIN){
             if(!sec.CheckPIN(PIN,user))return;
-            user.newTransaction();
             if(sec.check_Withdraw(amount,user.getBalance())){
-                user.decrease(amount,user);
+                user.decrease(amount);
                 Transaction transaction=new Transaction("Withdrawal",amount,user);
                 user.AddTransaction(transaction);
                 return;
@@ -21,8 +20,8 @@
          void deposit (double amount,Security sec,User_Details user,String PIN){
              if(!sec.CheckPIN(PIN,user))return;
              if(sec.check_Deposit(amount)){
-                user.increase(amount,user);
-                Transaction transaction=new Transaction("Withdrawal",amount,user);
+                user.increase(amount);
+                Transaction transaction=new Transaction("deposit",amount,user);
                 user.AddTransaction(transaction);
                 return;
             }
@@ -33,16 +32,14 @@
          void transfer(double amount,Security sec,User_Details usersend,String receive_AccNum,String PIN,UsersRepo repo){
              //user send
             if(!sec.CheckPIN(PIN,usersend))return;
-            usersend.newTransaction();
             Transaction transaction=new Transaction("Transfer \'send\'",amount,usersend);
             usersend.AddTransaction(transaction);
-            usersend.decrease(amount,usersend);
+            usersend.decrease(amount);
             //user receives
              User_Details reference=repo.GetUser_Accountnumber(receive_AccNum);
-             reference.newTransaction();
-             Transaction transaction2=new Transaction("Transfer \'receive\'",amount,usersend);
-             usersend.AddTransaction(transaction2);
-             usersend.increase(amount,reference);
+             Transaction transaction2=new Transaction("Transfer \'receive\'",amount,reference);
+             reference.AddTransaction(transaction2);
+             usersend.increase(amount);
          }
 
 
