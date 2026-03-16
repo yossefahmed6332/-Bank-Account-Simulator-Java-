775cfd5bc87bef9a025c044da3412baa6be02363
yossef <yossefahmed6332@gmail.com>
2026-03-16 15:47:40 +0200

    new class
    Add Admin class , objects is admin who control all data

---
+++ Admin.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Admin extends User{
+     Admin(String ID, String username,String email, String password, Address address)
+    {
+        super(ID,username,email,password,address);
+    }
+
+}
