45dfbf06af025d4880ea902bcbf17bf692a25d47
yossef <yossefahmed6332@gmail.com>
2026-03-17 17:58:54 +0200

    Update Admin_Service.java

--- Admin_Service.java
+++ Admin_Service.java
@@ -1,6 +1,6 @@
 public class Admin_Service {
     //Admin service
-    void getUser_ByID(String ID,UsersRepo rep){
+    static void getUser_ByID(String ID,UsersRepo rep){
         User_Details user=rep.GetUser_ID(ID);
         if(user!=null){
             System.out.println(user.toString());
@@ -9,7 +9,7 @@ public class Admin_Service {
             System.out.println("User not found");
         }
     }
-    void getUser_ByUsername(String username,UsersRepo rep){
+    static void getUser_ByUsername(String username,UsersRepo rep){
         User_Details user=rep.GetUser_UserName(username);
         if(user!=null){
             System.out.println(user.toString());
@@ -18,7 +18,7 @@ public class Admin_Service {
             System.out.println("User not found");
         }
     }
-    void getUser_ByEmail(String email,UsersRepo rep){
+    static void getUser_ByEmail(String email,UsersRepo rep){
         User_Details user=rep.GetUser_Email(email);
         if(user!=null){
             System.out.println(user.toString());
@@ -27,12 +27,12 @@ public class Admin_Service {
             System.out.println("User not found");
         }
     }
-    void getAll_users(UsersRepo rep){
+    static void getAll_users(UsersRepo rep){
         for(User_Details user:rep.getUsers()){
             System.out.println(user.toString());
         }
     }
-    void removeUser(String ID,UsersRepo rep){
+    static void removeUser(String ID,UsersRepo rep){
         rep.deleteUser(ID);
     }
 
