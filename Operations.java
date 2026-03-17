4ed622762aa4481d945af7ae0af954c18cfba4a6
yossef <yossefahmed6332@gmail.com>
2026-03-17 20:39:37 +0200

    Update Operations.java
    All methods is static right now

--- Operations.java
+++ Operations.java
@@ -1,54 +1,61 @@
+import java.util.ArrayList;
 
-    class Operations{
-
-
-
+class Operations{
 
 
-        public void withdraw(double amount,Security sec,User_Details user,String PIN){
-            if(!sec.CheckPIN(PIN,user))return;
-            user.newTransaction();
-            if(sec.check_Withdraw(amount,user.getBalance())){
-                user.decrease(amount,user);
-                Transaction transaction=new Transaction("Withdrawal",amount,user);
-                user.AddTransaction(transaction);
-                return;
-            }
-            System.out.println("invalid operation");
+
+
+
+    static void withdraw(double amount,User_Details user,String PIN){
+        if(!Security.CheckPIN(PIN,user)){
+            System.out.println("invalid operation,Wrong PIN");
             return;
         }
-
-         void deposit (double amount,Security sec,User_Details user,String PIN){
-             if(!sec.CheckPIN(PIN,user))return;
-             if(sec.check_Deposit(amount)){
-                user.increase(amount,user);
-                Transaction transaction=new Transaction("Withdrawal",amount,user);
-                user.AddTransaction(transaction);
-                return;
-            }
-             System.out.println("invalid operation");
-             return;
-            }
-
-         void transfer(double amount,Security sec,User_Details usersend,String receive_AccNum,String PIN,UsersRepo repo){
-             //user send
-            if(!sec.CheckPIN(PIN,usersend))return;
-            usersend.newTransaction();
-            Transaction transaction=new Transaction("Transfer \'send\'",amount,usersend);
-            usersend.AddTransaction(transaction);
-            usersend.decrease(amount,usersend);
-            //user receives
-             User_Details reference=repo.GetUser_Accountnumber(receive_AccNum);
-             reference.newTransaction();
-             Transaction transaction2=new Transaction("Transfer \'receive\'",amount,usersend);
-             usersend.AddTransaction(transaction2);
-             usersend.increase(amount,reference);
-         }
-
-
-
-
-
-
+        if(Security.check_Withdraw(amount,user.getBalance())){
+            user.decrease(amount);
+            Transaction transaction=new Transaction("Withdrawal",amount,user);
+            user.AddTransaction(transaction);
+            return;
+        }
+        System.out.println("invalid operation");
+        return;
     }
+
+    static void deposit (double amount,User_Details user,String PIN){
+        if(!Security.CheckPIN(PIN,user)){
+            System.out.println("invalid operation,Wrong PIN");
+            return;
+        }
+        if(Security.check_Deposit(amount)){
+            user.increase(amount);
+            Transaction transaction=new Transaction("deposit",amount,user);
+            user.AddTransaction(transaction);
+            return;
+        }
+        System.out.println("invalid operation");
+        return;
+    }
+
+    static void transfer(double amount,User_Details usersend,String receive_AccNum,String PIN,UsersRepo repo){
+        //user send
+        if(!Security.CheckPIN(PIN,usersend)){
+            System.out.println("invalid operation,Wrong PIN");
+            return;
+        }
+        Transaction transaction=new Transaction("Transfer \'send\'",amount,usersend);
+        usersend.AddTransaction(transaction);
+        usersend.decrease(amount);
+        //user receives
+        User_Details reference=repo.GetUser_Accountnumber(receive_AccNum);
+        Transaction transaction2=new Transaction("Transfer \'receive\'",amount,reference);
+        reference.AddTransaction(transaction2);
+        reference.increase(amount);
+    }
+
+
+
+
+
+
+}
 
