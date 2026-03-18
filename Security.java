a1a4778cea31f80feb6ae44a07c6171f93118596
yossef <yossefahmed6332@gmail.com>
2026-03-18 16:42:27 +0200

    Update Security.java

--- Security.java
+++ Security.java
@@ -6,7 +6,7 @@ public class Security {
      static boolean CheckPassword(String Password,User_Details user){
         return Password. equals(user.getPassword());
     }
-     boolean CheckPIN (String PIN , User_Details user){
+     static boolean CheckPIN (String PIN , User_Details user){
         return PIN.equals(user.getPIN());
     }
 
@@ -17,16 +17,49 @@ public class Security {
     static boolean check_Deposit(double amount){
          return amount >=0;
     }
-    static boolean Repeated(String checked, UsersRepo repository) {
+    static boolean Repeated_Accnum(String checked, UsersRepo repository) {
 
         for (User_Details user : repository.getUsers()) {
             if (checked.equals(user.getAccount_number())) return true;
-            if (checked.equals(user.getPIN())) return true;
-            if (checked.equals(user.getID())) return true;
-            if (checked.equals(user.getUserName())) return true;
         }
 
         return false;
+    }
+    static boolean Repeated_ID(String checked, UsersRepo repository) {
+         for (User_Details user : repository.getUsers()) {
+             if (checked.equals(user.getID())) return true;
+         }
+         return false;
+    }
+    static boolean Repeated_Email(String checked, UsersRepo repository) {
+         for (User_Details user : repository.getUsers()) {
+             if (checked.equals(user.getEmail())) return true;
+         }
+         return false;
+    }
+    static boolean Repeated_PIN(String checked, UsersRepo repository) {
+         for (User_Details user : repository.getUsers()) {
+             if (checked.equals(user.getPIN())) return true;
+         }
+         return false;
+    }
+    static boolean Repeated_Username(String checked, UsersRepo repository) {
+         for (User_Details user : repository.getUsers()) {
+             if (checked.equals(user.getUserName())) return true;
+         }
+         return false;
+    }
+    static boolean transfer(User_Details userSend,User_Details userReceive,double amount){
+         //check amount sent
+        if(amount>userSend.getBalance())return false;
+        //check userReceive if exist
+        if(userReceive==null)return false;
+        //accessing security system
+        return true;
+
+
+
+
     }
 
 
