fdeb596b12b7ccb37fe13b7f0ec0a21660406acd
yossef <yossefahmed6332@gmail.com>
2026-03-18 16:43:00 +0200

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -8,7 +8,6 @@ class Operations{
 
     static void withdraw(double amount,User_Details user,String PIN){
         if(!Security.CheckPIN(PIN,user)){
-            System.out.println("invalid operation,Wrong PIN");
             return;
         }
         if(Security.check_Withdraw(amount,user.getBalance())){
@@ -17,13 +16,11 @@ class Operations{
             user.AddTransaction(transaction);
             return;
         }
-        System.out.println("invalid operation");
         return;
     }
 
     static void deposit (double amount,User_Details user,String PIN){
         if(!Security.CheckPIN(PIN,user)){
-            System.out.println("invalid operation,Wrong PIN");
             return;
         }
         if(Security.check_Deposit(amount)){
@@ -32,21 +29,23 @@ class Operations{
             user.AddTransaction(transaction);
             return;
         }
-        System.out.println("invalid operation");
         return;
     }
 
     static void transfer(double amount,User_Details usersend,String receive_AccNum,String PIN,UsersRepo repo){
-        //user send
-        if(!Security.CheckPIN(PIN,usersend)){
-            System.out.println("invalid operation,Wrong PIN");
-            return;
-        }
+        //user send operations
+        if(!Security.CheckPIN(PIN,usersend))return;//check PIN
+
         Transaction transaction=new Transaction("Transfer \'send\'",amount,usersend);
         usersend.AddTransaction(transaction);
         usersend.decrease(amount);
-        //user receives
-        User_Details reference=repo.GetUser_Accountnumber(receive_AccNum);
+
+
+        //user received operations
+        User_Details reference=repo.getUser_ByAccountnumber(receive_AccNum);
+        if(!Security.transfer(usersend,reference,amount))return;//check
+
+
         Transaction transaction2=new Transaction("Transfer \'receive\'",amount,reference);
         reference.AddTransaction(transaction2);
         reference.increase(amount);
