892c0028be98a44928d3c47d05325caf37e3e200
yossef <yossefahmed6332@gmail.com>
2026-03-19 03:57:27 +0200

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,22 +1,22 @@
 public class Login {
 
-    User_Details login_Byemail(String email, String password  ,UsersRepo rep) {
+    static User_Details login_Byemail(String email, String password  ,UsersRepo rep) {
         for (User_Details user : rep.getUsers()) {
             if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
                 return user;
             }
-            System.out.println("Invalid email or Password");
 
         }
         return null;
     }
 
 
-    User_Details login_ByuserName( String password,String username,UsersRepo rep){
+    static User_Details login_ByuserName(String username, String password, UsersRepo rep) {
         for (User_Details user : rep.getUsers()) {
-            if (user.getUserName().equals(username) && user.getPassword().equals(password)) return user;
+            if (user.getUserName().equals(username) && user.getPassword().equals(password)) {
+                return user;
+            }
         }
-        System.out.println("Invalid Username or Password");
         return null;
     }
 
